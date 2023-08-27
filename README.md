0fb7e25db34e86ed15554aafbcc5b06c86134307
Andranik Arakelyan <andranik.arakelyan.work@gmail.com>
2023-08-27 12:13:24 +0400

    Implemented database models.

--- README.md
+++ README.md
@@ -11,13 +11,20 @@ docker-compose up -d --build
 
 You can view and change all environment variables in file *.env*. Default config is
 ```shell
+# Server port. Access via http://localhost:{SERVER_PORT}
 SERVER_PORT=4000
 
+# Server's credentials to connect to database
 POSTGRES_USER=postgres
 POSTGRES_PASSWORD=postgres
 POSTGRES_DB=volt-task-db
-POSTGRES_HOST=volt-task-db # use 'localhost' to access from outside
-POSTGRES_PORT=4001
+POSTGRES_HOST=volt-task-postgres
+POSTGRES_PORT=5432
+
+# For development purposes, "public" port of postgres inside the docker.
+# To access from "outside" connect to localhost:{POSTGRES_PUBLIC_PORT}.
+# Username, password, db you can get from variables above
+POSTGRES_PUBLIC_PORT=4001
 ```
 
 ### How to run server in local
--- src/db/DbClient.ts
+++ src/db/DbClient.ts
@@ -1,26 +1,111 @@
-import {Sequelize} from "sequelize";
+import {DataTypes, Sequelize} from "sequelize";
 import {AppConfig} from "../config";
 
 export class DbClient {
 
-  private static _sequalize: Sequelize;
-
-  public static init() {
-    this._sequalize = new Sequelize({
-      dialect: 'postgres',
-      host: AppConfig.db.host,
-      port: 5432,
-      username: AppConfig.db.user,
-      password: AppConfig.db.password,
-      database: AppConfig.db.db,
-    });
-  }
+  public static sequalize: Sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: AppConfig.db.host,
+    port: AppConfig.db.port,
+    username: AppConfig.db.user,
+    password: AppConfig.db.password,
+    database: AppConfig.db.db,
+  });
 
   public static async connect() {
-    await new Promise( resolve => setTimeout(resolve, 10_000) )
-    await this._sequalize.authenticate();
+    await this.sequalize.authenticate();
+    await this.sequalize.sync({alter: true});
   }
 
 }
 
-DbClient.init();
+//FIXME: Nicely move to another folder and create nice model loading system
+export const PostEntity = DbClient.sequalize.define(
+  'Post',
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.INTEGER,
+    },
+    published_at: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: 'posts',
+  }
+);
+
+//FIXME: Nicely move to another folder and create nice model loading system
+export const CommentEntity = DbClient.sequalize.define(
+  'Comment',
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.INTEGER,
+    },
+    published_at: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: 'comments',
+  }
+);
+
+//FIXME: Nicely move to another folder and create nice model loading system
+export const UserEntity = DbClient.sequalize.define(
+  'User',
+  {
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'users',
+  }
+);
+
+//FIXME: Nicely move to another folder and create nice model loading system
+export const ChangeEntity = DbClient.sequalize.define(
+  'Change',
+  {
+    table_name: {
+      type: DataTypes.STRING,
+    },
+    table_id: {
+      type: DataTypes.INTEGER,
+    },
+    changed_at: {
+      type: DataTypes.DATE,
+    },
+    data: {
+      type: DataTypes.JSONB,
+    },
+    action: {
+      type: DataTypes.ENUM('insert', 'delete'),
+    },
+  },
+  {
+    tableName: 'changes',
+  },
+);
